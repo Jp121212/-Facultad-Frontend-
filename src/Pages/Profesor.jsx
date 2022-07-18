6162547b4d7e2a102e487c00dc9f1304cea37d37
Jp121212 <jose.elizondo@unadeca.net>
2022-07-18 17:08:38 -0600

    Edits en rutas profesor aplicando estilos

--- src/Pages/Profesor.jsx
+++ src/Pages/Profesor.jsx
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { useParams  } from 'react-router-dom';
 import axios from 'axios';
+import Header from '../Components/Header';
+import Footer from '../Components/Footer';
 
 const Profesor = () => {
   const [ProfesorActual, setProfesor] = useState(null);
@@ -15,23 +17,61 @@ const Profesor = () => {
   }, [])
   console.log(ProfesorActual)
   return(
+    <div className='.mainDiv'>
+        <Header/>
     <div>
-      {
-        ProfesorActual ?
-        <div>
-          <p>Nombre: {ProfesorActual.nombre} {ProfesorActual.apellido}</p>
-          <p>Facultad: {ProfesorActual.facultad_id}</p>
-          <p>Materias:</p>
-          <ul>
-            {
-              ProfesorActual.materias.map((el) => <div>
-                <li>{el.nombre} • Codigo: {el.codigo} • Creditos {el.creditos} •  {el.carrera}</li>
-              </div>)
-              }
+    <div className='raya'></div>     
+    <div className='banner1'>
+        <img  src='http://imgs.globovision.com/8VElMcM2kawSaM4RrffQrfDqa-k=/847x0/smart/59560fd968e94056bea61a7ef92f07f0' className='ban11'></img>
+        <div className="baninfo">
+              <h1 className='h0'>Contamos con los mejores Profesionales</h1>
+              <p>Cada profesor es especializado en su Area y se encargara de compartir su conocimiento con el estudiante ademas de darle una atencion de calidad</p>
+                <button className="button5">Contactar</button>
+            </div></div>
+        <div className='Contendor'>
+          <div className='Contenedor4'>
+             <div className='cuadro'>
+              <h1 className='h10'>Profesor:</h1>
+             <img className='img6' src='https://us.123rf.com/450wm/rastudio/rastudio1708/rastudio170808151/84739872-icono-de-l%C3%ADnea-de-vector-de-profesor-aislado-sobre-fondo-blanco-icono-de-l%C3%ADnea-de-profesor-para-info.jpg'>
+             </img>{
+              
+              ProfesorActual ?
+              <div>
+                <p>Nombre: {ProfesorActual.nombre} {ProfesorActual.apellido}</p>
+                <p>Facultad: {ProfesorActual.facultad_id}</p>
+                <p>Materias:</p>
+                <ul>
+                  {
+                    ProfesorActual.materias.map((el) => <div>
+                      <div>
+                        <li>Nombre: {el.nombre}</li>
+                        <li>Carrera: {el.carrera}</li>
+                        <li>Creditos: {el.creditos}</li>
+                      </div>
+                    </div>)
+                    }
           </ul>
         </div>
         : 'Loading...'
       }
+             </div>
+            </div>
+            <div className='Contenedor4'>
+            <h1 className='h10'>Origen</h1>
+            <hr></hr>
+            <p>
+            El origen de la Universidad Adventista de Centro América se remonta al año 1925, cuando la Iglesia Adventista del Séptimo Día estableció un Centro Educativo en la comunidad de las Cascadas, corregimiento de Pedregal, en la ciudad capital de Panamá. Digno de recordar al Pastor William Baxter quien propuso la fundación de una casa de estudios secundarios que gradualmente crecería y llegara a convertirse en columna y vida para la educación y la formación de la entusiasta juventud centroamericana. Con fe sencilla pero aferrada al brazo de la Omnipotencia, se da inicio a ese sueño con una significativa ceremonia en la que se mezclaron el canto y la plegaria, la emoción y el gozo, la audacia y la confianza, el ideal y el triunfo. Así nace la institución con el nombre descriptivo de: “Escuela Para Jóvenes de Habla Española”
+            </p>
+            <h1 className='h10'>Dato Curioso</h1>
+            <hr></hr>
+            <p>Bajo la dirección de C. F. Montgomery; en 1945 se tomó la decisión de cambiarle el nombre a la institución de “Academia Hispanoamericana Adventista” a COVAC (Colegio Vocacional Adventista de América Central). En 1948 se organizó la Asociación Estudiantil; en consecuencia da inicio la publicación de Eco Estudiantil, el cual ha sido desde entonces casi la única fuente de información para conocer la historia de este centro de estudios.  </p>
+            </div>
+
+        </div>
+     
+    </div> 
+
+    <Footer/>
     </div>
   )
 }
--- src/Pages/Profesores.jsx
+++ src/Pages/Profesores.jsx
@@ -4,6 +4,9 @@ import axios from 'axios';
 import {
   Outlet
 } from 'react-router-dom'
+import Header from '../Components/Header';
+import Accordion3 from '../Components/Accordion3';
+import Footer from '../Components/Footer';
 
 const Profesores = () => {
   const [Profesor, setProfesor] = React.useState(null);
@@ -23,97 +26,54 @@ const Profesores = () => {
     setProfesorActivo(Profesor.find((el) => el.id === id));
   }
 
-  const agregarFavorito = () => {
-    // console.log(localStorage.getItem('favoritos'))
-    if(!localStorage.getItem('favoritos')){
-      // case, no existe aun
-      let data = JSON.stringify([ProfesorActivo]);
-      localStorage.setItem('favoritos', data)
-      setFavoritos([ProfesorActivo]);
-    } else{
-      // patron inmutable de actualizar un array
-      let data = JSON.parse(localStorage.getItem('favoritos'))
-      let newData = JSON.stringify([...data, ProfesorActivo]);
-      localStorage.setItem('favoritos', newData)
-      let tempFavoritos = [...favoritos, ProfesorActivo];
-      setFavoritos(tempFavoritos);
-    }
-  }
-
-  const eliminarFavorito = (id) => {
-    console.log({id})
-    let temp_favoritos = [...favoritos]
-    let nuevos_favoritos = temp_favoritos.filter((el) => el.id !== id);
-    console.log(nuevos_favoritos);
-    localStorage.setItem('favoritos', JSON.stringify(nuevos_favoritos))
-    setFavoritos(nuevos_favoritos)
-  }
-  
-  const esFav = (id) => {
-    let exists = ((el) => el.id === id);
-    if(exists.length > 0){
-      return true
-    } else{
-      return false
-    }
-  }
+ 
 
 
 
 
 
   
-  console.log({favoritos})
+ 
   return(
-    <div>
-      <Titulo>Profesores</Titulo>
-      <ul>
-        {
-          Profesor ? 
-            Profesor.map((el, i) => <div>
-              <li key={i} onClick={() => manejarClickLista(el.id)} style={{
-              cursor: 'pointer'
-            }}>{el.nombre} {el.apellido}</li> 
-      
-              </div>) : 
-          'Loading...'
-        }
-      </ul>
+    <div className='.mainDiv'>
+        <Header/>
+      <div className='raya'></div>     
+      <div className='banner1'> 
+          <img className='ban11' src='https://scontent.fsjo15-1.fna.fbcdn.net/v/t39.30808-6/241515621_6194628477279160_8513415961040256473_n.jpg?_nc_cat=111&ccb=1-7&_nc_sid=730e14&_nc_ohc=Keh7f3s0OKMAX9dDI5x&_nc_ht=scontent.fsjo15-1.fna&oh=00_AT_35RSuKIzrkoZCjX7h2PWbIH9nILyJCMoZ3Sy2AZ-OoA&oe=62DA5FBC'></img>
+          <div className="baninfo">
+              <h1 className='h0'>Contamos con los mejores Profesionales</h1>
+              <p>Cada profesor es especializado en su Area y se encargara de compartir su conocimiento con el estudiante ademas de darle una atencion de calidad</p>
+                <button className="button5">Contactar</button>
+            </div>    
+      </div>   
+      <div className='Contendor'>
+         <div className='Contenedor9'>
+    
+          <img className='img8' src='https://scontent.fsjo15-1.fna.fbcdn.net/v/t1.6435-9/209655752_5901701753238502_1570173186011512223_n.jpg?_nc_cat=108&ccb=1-7&_nc_sid=730e14&_nc_ohc=7HZW37jX9D0AX8JKWa6&_nc_ht=scontent.fsjo15-1.fna&oh=00_AT-J-R2Zok9A4YN2IdS4cR5bq_gCIrcXQa6gCsEHnftrtQ&oe=62FA45DA'></img>
+          </div>
 
-      {
-        ProfesorActivo &&
-        <div>
-          <p>Nombre: {ProfesorActivo.nombre} {ProfesorActivo.apellido}</p>
-          <p>Facultad: {ProfesorActivo.facultad_id}</p>
-          <p>Materias:</p>
-          <ul>
+
+
+         <div className='Contenedor8'>
+          <h1 className='h11'>Nuestros Profesores</h1>
             {
-              ProfesorActivo.materias.map((el) => <div>
-                <li>{el.nombre}</li>
-              </div>)
-              }
-          </ul>
-        <button onClick={agregarFavorito}> ✰</button>
-      </div>
-      }
-      <hr />
-      <h2>Mis favoritos</h2>
-      <ul>
-        {
-          favoritos ? 
-            favoritos.map((el, i) => <div key={i}>
-            <li 
-              style={{
-                cursor: 'pointer'
-              }}
-              onClick={() => eliminarFavorito(el.id)}
-            >{el.nombre ? el.nombre : "No hay Fav"}</li>
-            <button onClick={() => eliminarFavorito(el.id)}>🗑</button>
-            </div>) : 
-          'Loading...'
-        }
-      </ul>
-      <Outlet />
+            Profesor ?
+            Profesor.map((u,i)=>{ 
+              return(
+                <div key={i}>
+                  <Accordion3 pop={u}/> 
+                </div>
+              ) 
+
+            }): 'Loading...'
+          }
+           </div> 
+        
+        
+        </div>
+    
+      <Footer/>
+     
     </div>
   );
 }
